2fb303f3799abd8b8a53d9af7a02117664374d71
adantl <adantoscano@gmail.com>
2016-06-09 17:14:35 +0200

    New home activity. Attempt 1.

--- e-quilibrium/app/src/main/java/com/app/sirdreadlocks/e_quilibrium/MainActivity.java
+++ e-quilibrium/app/src/main/java/com/app/sirdreadlocks/e_quilibrium/MainActivity.java
@@ -1,67 +1,30 @@
 package com.app.sirdreadlocks.e_quilibrium;
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
-/*public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = (Button)findViewById(R.id.startButton);
+
+        btnStart.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+
+                Intent intent =
+                        new Intent(MainActivity.this, Measures.class);
+
+                startActivity(intent);
+            }
+        });
     }
-}*/
-
-public class MainActivity extends AppCompatActivity {
-    TextView textX, textY, textZ;
-    SensorManager sensorManager;
-    Sensor sensor;
-
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-
-        textX = (TextView) findViewById(R.id.textX);
-        textY = (TextView) findViewById(R.id.textY);
-        textZ = (TextView) findViewById(R.id.textZ);
-    }
-
-    public void onResume() {
-        super.onResume();
-        sensorManager.registerListener(gyroListener, sensor,
-                SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    public void onStop() {
-        super.onStop();
-        sensorManager.unregisterListener(gyroListener);
-    }
-
-    public SensorEventListener gyroListener = new SensorEventListener() {
-        public void onAccuracyChanged(Sensor sensor, int acc) { }
-
-        public void onSensorChanged(SensorEvent event) {
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
-            String strX = "X : " + (int)x + " rad/s";
-            String strY = "Y : " + (int)y + " rad/s";
-            String strZ = "Z : " + (int)z + " rad/s";
-
-
-
-            textX.setText(strX);
-            textY.setText(strY);
-            textZ.setText(strZ);
-        }
-    };
 }
---
+++ e-quilibrium/app/src/main/java/com/app/sirdreadlocks/e_quilibrium/Measures.java
@@ -0,0 +1,67 @@
+package com.app.sirdreadlocks.e_quilibrium;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/*public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}*/
+
+public class Measures extends AppCompatActivity {
+    TextView textX, textY, textZ;
+    SensorManager sensorManager;
+    Sensor sensor;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_measures);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        textX = (TextView) findViewById(R.id.textX);
+        textY = (TextView) findViewById(R.id.textY);
+        textZ = (TextView) findViewById(R.id.textZ);
+    }
+
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(gyroListener, sensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void onStop() {
+        super.onStop();
+        sensorManager.unregisterListener(gyroListener);
+    }
+
+    public SensorEventListener gyroListener = new SensorEventListener() {
+        public void onAccuracyChanged(Sensor sensor, int acc) { }
+
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            String strX = "X : " + (int)x + " rad/s";
+            String strY = "Y : " + (int)y + " rad/s";
+            String strZ = "Z : " + (int)z + " rad/s";
+
+
+
+            textX.setText(strX);
+            textY.setText(strY);
+            textZ.setText(strZ);
+        }
+    };
+}

d75f3c0eef34b781f3c0b6b371f8e90b234b0a76
adantl <adantoscano@gmail.com>
2016-09-11 15:41:40 +0200

    New gitignore

---
+++ e-quilibriumApp/app/src/test/java/com/app/sirdreadlocks/e_quilibrium/ExampleUnitTest.java
@@ -0,0 +1,19 @@
+package com.app.sirdreadlocks.e_quilibrium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    public void helloTest(){
+        String hi = "Hello Test";
+    }
+}

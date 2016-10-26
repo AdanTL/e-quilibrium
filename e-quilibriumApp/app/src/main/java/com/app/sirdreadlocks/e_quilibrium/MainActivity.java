e1fc571dc9529f7da55888b433414a8562e3e347
AdanTL <adantoscano@gmail.com>
2016-10-26 17:49:39 +0200

    FirebaseUI Google and Facebook auth working

--- e-quilibriumApp/app/src/main/java/com/app/sirdreadlocks/e_quilibrium/MainActivity.java
+++ e-quilibriumApp/app/src/main/java/com/app/sirdreadlocks/e_quilibrium/MainActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+
+import static com.firebase.ui.auth.ui.AcquireEmailHelper.RC_SIGN_IN;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,8 +24,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // already signed in
+            Toast.makeText(getApplicationContext(), "Welcome again "+auth.getCurrentUser().getDisplayName()+"!", Toast.LENGTH_SHORT).show();
+        } else {
+            // not signed in
+            startActivityForResult(
+                    // Get an instance of AuthUI based on the default app
+                    AuthUI.getInstance().createSignInIntentBuilder()
+                            .setIsSmartLockEnabled(false)
+                            .setProviders(
+                                    AuthUI.EMAIL_PROVIDER,
+                                    AuthUI.GOOGLE_PROVIDER,
+                                    AuthUI.FACEBOOK_PROVIDER)
+                            .setTheme(R.style.GreenTheme).build(),
+                    RC_SIGN_IN);
+        }
+
         btnNewPat = (Button) findViewById(R.id.btnNewPat);
-        btnSignOut = (Button) findViewById(R.id.btnListPat);
+        btnSignOut = (Button) findViewById(R.id.btnSignOut);
 
 
         btnNewPat.setOnClickListener(new View.OnClickListener() {
@@ -43,8 +65,15 @@ public class MainActivity extends AppCompatActivity {
                         .addOnCompleteListener(new OnCompleteListener<Void>() {
                             public void onComplete(@NonNull Task<Void> task) {
                                 // user is now signed out
-                                startActivity(new Intent(MainActivity.this, Authentication.class));
-                                finish();
+                                startActivityForResult(
+                                        AuthUI.getInstance().createSignInIntentBuilder()
+                                                .setIsSmartLockEnabled(false)
+                                                .setProviders(
+                                                    AuthUI.EMAIL_PROVIDER,
+                                                    AuthUI.GOOGLE_PROVIDER,
+                                                    AuthUI.FACEBOOK_PROVIDER)
+                                                .setTheme(R.style.GreenTheme).build(),
+                                        RC_SIGN_IN);
                             }
                         });
             }
@@ -52,17 +81,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void onClick(View v) {
-        if (v.getId() == R.id.btnListPat) {
-            AuthUI.getInstance()
-                    .signOut(this)
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                        public void onComplete(@NonNull Task<Void> task) {
-                            // user is now signed out
-                            startActivity(new Intent(MainActivity.this, Authentication.class));
-                            finish();
-                        }
-                    });
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                // user is signed in!
+                Toast.makeText(getApplicationContext(), "Welcome!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Try again", Toast.LENGTH_SHORT).show();
+                startActivityForResult(
+                        AuthUI.getInstance().createSignInIntentBuilder()
+                                .setIsSmartLockEnabled(false)
+                                .setProviders(
+                                        AuthUI.EMAIL_PROVIDER,
+                                        AuthUI.GOOGLE_PROVIDER,
+                                        AuthUI.FACEBOOK_PROVIDER)
+                                .setTheme(R.style.GreenTheme).build(),
+                        RC_SIGN_IN);
+            }
         }
     }
+
 }
